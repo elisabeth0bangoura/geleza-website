8a98d4d28dcb43ce357a6d4d05b20fbb1b50413c
Phumudzo Mahandana <phumudzo@soimagine.co.za>
2023-01-20 19:19:22 +0200

    Init

---
+++ app/how-it-works/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PublicLayout from '../../components/main/PublicLayout'
+import CallToAction from '../../components/public/CallToAction'
+
+const Page = () => {
+  return (
+    <PublicLayout>
+      <div>
+        <CallToAction />
+      </div>
+    </PublicLayout>
+  )
+}
+
+export default Page

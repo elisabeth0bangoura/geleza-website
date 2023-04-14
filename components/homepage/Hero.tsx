60cf05726d216a32b56b3d4bf98b99fef3cfb983
Phumudzo Mahandana <phumudzo@soimagine.co.za>
2023-04-14 12:29:28 +0200

    Final Update - Before library & groups

--- components/homepage/Hero.tsx
+++ components/homepage/Hero.tsx
@@ -8,19 +8,19 @@ const Hero = () => {
     <div className="bg-gradient-to-tr from-gray-800 via-cyan-900 to-purple-900">
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-10 px-4 mx-auto max-w-7xl py-20">
         <div>
-          <Badge size="xl" color="orange" className="bg-teal-500 text-white">
+          <Badge size="xl" color="orange" className="bg-teal-500 text-white capitalize">
             High School & College Students
           </Badge>
           <br />
           <h1
-            className="text-5xl md:text-6xl font-black mb-7 mt-6 text-white"
+            className="text-4xl md:text-6xl font-black mb-7 mt-6 text-white"
             style={{
               textShadow: '2px 2px 4px rgba(0,0,0,0.1)',
             }}
           >
-            #1 🎉
+            
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600">
-              A.I Powered
+             #1 A.I Powered
             </span>{' '}
             Students Tools{' '}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-blue-600">
@@ -37,7 +37,7 @@ const Hero = () => {
               target="_blank"
               className="btn border-2 rounded-full bg-white px-24 border-white  text-dark text-lg font-bold"
             >
-             Start For Free
+             Start For Free 🎉
             </a>
           </div>
          
